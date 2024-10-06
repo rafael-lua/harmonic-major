4fed442136db7bc5a0277e12a2bca170fadf1eb7
rafael-lua <rafael.tragueta79@gmail.com>
2024-10-05 23:32:57 -0300

    feat: configure push

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,43 @@
-import { info, setFailed, setOutput } from "@actions/core"
+import { getInput, info, setFailed, setOutput } from "@actions/core"
+import { exec } from "@actions/exec"
+import { context } from "@actions/github"
 import release from "./core/release"
 
 const run = async () => {
     try {
         info("Release action started...")
 
+        const githubToken = getInput("github-token")
+
         const newRelease = await release()
 
         if (newRelease) {
             setOutput("release", newRelease)
             info("New release created!")
+
+            await exec("git", [
+                "config",
+                "--global",
+                "user.email",
+                '"github-actions[bot]@users.noreply.github.com"',
+            ])
+            await exec("git", [
+                "config",
+                "--global",
+                "user.name",
+                '"github-actions[bot]"',
+            ])
+
+            await exec("git", [
+                "remote",
+                "set-url",
+                "origin",
+                `https://x-access-token:${githubToken}@github.com/${context.repo.owner}/${context.repo.repo}.git`,
+            ])
+
+            await exec("git", ["push", "--follow-tags"])
+
+            info("New release pushed!")
         } else {
             info("No new release created!")
         }
