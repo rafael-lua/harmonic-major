f5499e593c5c4d377dbcd08ab6d0db00fc3967f2
rafael-lua <rafael.tragueta79@gmail.com>
2024-10-06 01:04:27 -0300

    update dist

--- unbuild.config.ts
+++ unbuild.config.ts
@@ -1,5 +1,15 @@
 import { defineBuildConfig } from "unbuild"
 
 export default defineBuildConfig({
+    failOnWarn: false,
+    rollup: {
+        resolve: {
+            preferBuiltins: true,
+            moduleDirectories: ["node_modules"],
+        },
+        commonjs: {
+            include: ["node_modules/**"],
+        },
+    },
     externals: [],
 })
